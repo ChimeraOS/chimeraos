81d261f388ec5d371575069136db46d17f03f80b
Alesh Slovak <alesh@roadmunk.com>
2019-03-24 21:46:37 -0400

    initial installer script

---
+++ install.sh
@@ -0,0 +1,111 @@
+#! /bin/bash
+
+set -e
+
+export USERNAME=gamer
+export SYSTEM_NAME=gamer-os
+
+export MOUNT_PATH=/tmp/${SYSTEM_NAME}
+
+if [ -z $1 ]; then
+	echo "No install disk specified. Please specify a disk from one of the following:"
+	lsblk -p | grep disk | tr -s ' ' | cut -d' ' -f 1
+	exit
+fi
+
+export DISK=$1
+
+if ! file ${DISK} | grep block > /dev/null; then
+	echo "${DISK} is not a valid disk. Please specify a disk from one of the following:"
+	lsblk -p | grep disk | tr -s ' ' | cut -d' ' -f 1
+	exit
+fi
+
+read -p "WARNING: ${DISK} will now be formatted. All data on the disk will be lost. Do you wish to proceed? (y/n)" -n 1 -r
+echo
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+	echo "Aborting install"
+	exit
+fi
+
+echo 'start=2048, type=83' | sfdisk ${DISK}
+mkfs.ext4 -F ${DISK}1
+mkdir -p ${MOUNT_PATH}
+mount ${DISK}1 ${MOUNT_PATH}
+pacstrap ${MOUNT_PATH} base
+arch-chroot ${MOUNT_PATH} /bin/bash <<EOF
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
+mkinitcpio -p linux
+pacman --noconfirm -S grub
+
+# enable bluetooth connection for xbox one s controller
+sed -i 's/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="bluetooth.disable_ertm=1"/' /etc/default/grub
+
+grub-install --target=i386-pc ${DISK}
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+" >> /etc/pacman.conf
+
+pacman --noconfirm -Sy
+pacman --noconfirm -S \
+	lightdm \
+	accountsservice \
+	xorg-server \
+	bluez \
+	bluez-utils \
+	lib32-freetype2 \
+	lib32-curl \
+	lib32-libgpg-error \
+	ttf-dejavu \
+	networkmanager \
+	pulseaudio \
+	lib32-libpulse \
+	sudo \
+	python \
+	steam
+
+# install nvidia graphics if needed
+if lspci | grep -E -i '(vga|3d)' | grep -i nvidia > /dev/null; then
+	pacman --noconfirm -S nvidia nvidia-utils lib32-nvidia-utils
+fi
+
+systemctl enable NetworkManager
+systemctl enable lightdm
+systemctl enable bluetooth
+
+# font workaround for initial big picture mode startup
+mkdir -p /usr/share/fonts/truetype/ttf-dejavu
+ln -s /usr/share/fonts/TTF/DejaVuSans.ttf /usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf
+
+# install steam compositor (no updates!)
+curl -L http://github.com/alkazar/steamos-compositor/releases/download/1.1.1/steamos-compositor-plus-1.1.1-1-x86_64.pkg.tar -o steamos-compositor-plus-1.1.1-1-x86_64.pkg.tar
+pacman --noconfirm -U steamos-compositor-plus-1.1.1-1-x86_64.pkg.tar
+rm steamos-compositor-plus-1.1.1-1-x86_64.pkg.tar
+
+passwd -l root # disable root login
+groupadd -r autologin
+useradd -m ${USERNAME} -G autologin
+echo "${USERNAME}:${USERNAME}" | chpasswd
+echo "${USERNAME}   ALL=(ALL) ALL" >> /etc/sudoers
+
+echo "
+[LightDM]
+run-directory=/run/lightdm
+[Seat:*]
+session-wrapper=/etc/lightdm/Xsession
+autologin-user=${USERNAME}
+autologin-session=steamos
+" > /etc/lightdm/lightdm.conf
+
+echo "${SYSTEM_NAME}" > /etc/hostname
+
+# steam controller fix
+echo "blacklist hid_steam" > /etc/modprobe.d/blacklist.conf
+
+EOF
