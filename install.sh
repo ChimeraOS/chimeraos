e1cfc9cea57d87705923e899ef4f5a93b02fe437
Alesh Slovak <aleshslovak@gmail.com>
2019-04-01 23:58:07 -0400

    improved gpu autodetection and driver installation
    Co-authored-by: alwayslivid <alwayslivid@users.noreply.github.com>
    Co-authored-by: tokyoraven02 <tokyoraven02@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -51,17 +51,25 @@ else
 	mount ${DISK}1 ${MOUNT_PATH}
 fi
 
+# chroot into target
 pacstrap ${MOUNT_PATH} base
 arch-chroot ${MOUNT_PATH} /bin/bash <<EOF
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
+# initialize kernel module configuration file
+echo > /etc/modprobe.d/gameros.conf
+
+# adding multilib to pacman mirror list
 echo "
 [multilib]
 Include = /etc/pacman.d/mirrorlist
 " >> /etc/pacman.conf
 
+# update package databases
 pacman --noconfirm -Sy
+
+# basic package installation
 pacman --noconfirm -S \
 	lightdm \
 	accountsservice \
@@ -77,20 +85,61 @@ pacman --noconfirm -S \
 	lib32-libpulse \
 	sudo \
 	python \
-	vulkan-icd-loader \
-	lib32-vulkan-icd-loader \
-	vulkan-radeon \
-	lib32-vulkan-radeon \
 	efibootmgr \
 	grub \
 	steam
 
-# install nvidia graphics if needed
-if lspci | grep -E -i '(vga|3d)' | grep -i nvidia > /dev/null; then
-	pacman --noconfirm -S nvidia nvidia-utils lib32-nvidia-utils
-fi
-
 systemctl enable NetworkManager lightdm bluetooth
+
+
+# gpu detection/driver installation
+
+# NVIDIA
+if lspci | grep -E -i '(vga|3d|display)' | grep -i nvidia > /dev/null; then
+	echo "NVIDIA GPU detected, installing drivers..."
+	pacman --noconfirm -S \
+		nvidia \
+		nvidia-utils \
+		lib32-nvidia-utils
+
+# AMD
+elif lspci | grep -E -i '(vga|3d|display)' | grep -i amd > /dev/null; then
+	if lspci -nnk | grep -i vga -A3 | grep 'Kernel modules: radeon, amdgpu' > /dev/null; then
+		echo "AMD GPU with radeon kernel detected, switching kernel module and installing drivers..."
+		echo "blacklist radeon" >> /etc/modprobe.d/gameros.conf
+		echo "options amdgpu si_support=1" >> /etc/modprobe.d/gameros.conf
+		echo "options amdgpu cik_support=1" >> /etc/modprobe.d/gameros.conf
+		echo "options radeon si_support=0" >> /etc/modprobe.d/gameros.conf
+		echo "options radeon cik_support=0" >> /etc/modprobe.d/gameros.conf
+		pacman --noconfirm -S \
+			vulkan-icd-loader \
+			lib32-vulkan-icd-loader \
+			libva-mesa-driver \
+			lib32-libva-mesa-driver \
+			mesa-vdpau \
+			lib32-mesa-vdpau \
+			vulkan-radeon \
+			lib32-vulkan-radeon \
+			xf86-video-amdgpu
+	else
+		echo "Legacy AMD/ATI GPU detected, installing drivers..."
+		pacman --noconfirm -S \
+			xf86-video-ati \
+			mesa-vdpau \
+			lib32-mesa-vdpau
+	fi
+
+# Intel
+elif lspci | grep -E -i '(vga|3d|display)' | grep -i intel > /dev/null; then
+	echo "Intel GPU detected, installing drivers..."
+	pacman --noconfirm -S \
+		vulkan-intel \
+		lib32-vulkan-intel \
+		libva-intel-driver \
+		lib32-libva-intel-driver \
+		xf86-video-intel \
+		intel-media-driver
+fi
 
 # font workaround for initial big picture mode startup
 mkdir -p /usr/share/fonts/truetype/ttf-dejavu
@@ -101,7 +150,8 @@ curl -LO http://github.com/alkazar/steamos-compositor/releases/download/1.1.1/st
 pacman --noconfirm -U steamos-compositor-plus-1.1.1-1-x86_64.pkg.tar
 rm steamos-compositor-plus-1.1.1-1-x86_64.pkg.tar
 
-passwd -l root # disable root login
+# disable root login
+passwd -l root
 groupadd -r autologin
 useradd -m ${USERNAME} -G autologin
 echo "${USERNAME}:${USERNAME}" | chpasswd
@@ -119,12 +169,12 @@ autologin-session=steamos
 echo "${SYSTEM_NAME}" > /etc/hostname
 
 # steam controller fix
-echo "blacklist hid_steam" > /etc/modprobe.d/blacklist.conf
+echo "blacklist hid_steam" >> /etc/modprobe.d/gameros.conf
 
 # enable bluetooth connection for xbox one s controller
 sed -i 's/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="bluetooth.disable_ertm=1"/' /etc/default/grub
 
-# install bootloader
+# build initial ramdisk and install bootloader
 mkinitcpio -p linux
 
 if [ -d /sys/firmware/efi ]; then
