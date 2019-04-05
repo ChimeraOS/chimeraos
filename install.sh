f342573ebd76e8a5efd56c3678065742ff05a14c
Domagoj <49078290+tokyoraven02@users.noreply.github.com>
2019-04-05 13:36:43 +0200

    Fixed GPU support (#9)
    * Fixed GPU support
    
    * Update install.sh
    
    fixed some typing mistakes
    
    * Update install.sh

--- install.sh
+++ install.sh
@@ -108,7 +108,7 @@ if lspci | grep -E -i '(vga|3d|display)' | grep -i nvidia > /dev/null; then
 # AMD
 elif lspci | grep -E -i '(vga|3d|display)' | grep -i amd > /dev/null; then
 	if lspci -nnk | grep -i vga -A3 | grep 'Kernel modules: radeon, amdgpu' > /dev/null; then
-		echo "AMD GPU with radeon kernel detected, switching kernel module and installing drivers..."
+		echo "AMD graphics card with radeon kernel module detected, switching kernel module and installing drivers..."
 		echo "blacklist radeon" >> /etc/modprobe.d/gameros.conf
 		echo "options amdgpu si_support=1" >> /etc/modprobe.d/gameros.conf
 		echo "options amdgpu cik_support=1" >> /etc/modprobe.d/gameros.conf
@@ -124,6 +124,20 @@ elif lspci | grep -E -i '(vga|3d|display)' | grep -i amd > /dev/null; then
 			vulkan-radeon \
 			lib32-vulkan-radeon \
 			xf86-video-amdgpu
+	
+	elif lspci -nnk | grep -i vga -A3 | grep 'Kernel modules: amdgpu' > /dev/null; then
+		echo "AMD graphics card with amdgpu kernel module detected, installing drivers..."
+		pacman --noconfirm -S \
+			vulkan-icd-loader \
+			lib32-vulkan-icd-loader \
+			libva-mesa-driver \
+			lib32-libva-mesa-driver \
+			mesa-vdpau \
+			lib32-mesa-vdpau \
+			vulkan-radeon \
+			lib32-vulkan-radeon \
+			xf86-video-amdgpu		
+
 	else
 		echo "Legacy AMD/ATI GPU detected, installing drivers..."
 		pacman --noconfirm -S \
