12bf37292eb1c50eb5dd8cca4505f8dc858e5a76
Alesh Slovak <alesh@roadmunk.com>
2020-05-03 12:28:27 -0400

    test

--- build.sh
+++ build.sh
@@ -66,6 +66,9 @@ cp -R rootfs/. ${BUILD_PATH}/
 # chroot into target
 mount --bind ${BUILD_PATH} ${BUILD_PATH}
 arch-chroot ${BUILD_PATH} /bin/bash <<EOF
+set -e
+set -x
+
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
@@ -81,11 +84,9 @@ pacman --noconfirm -Sy
 
 # install packages
 pacman --noconfirm -S ${PACKAGES}
-rm -rf /var/cache/pacman/pkg/*
 
 # install AUR packages
 pacman --noconfirm -U /aur/*
-rm -rf /aur/*
 
 # record installed packages & versions
 pacman -Q > /manifest
