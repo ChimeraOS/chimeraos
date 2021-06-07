c34460843af4a0aed77ff2f5671016543b9b5778
Alesh Slovak <aleshslovak@gmail.com>
2021-06-07 19:40:17 -0400

    load manifest file in chroot

--- build.sh
+++ build.sh
@@ -69,13 +69,15 @@ if [ -n "${PACKAGE_OVERRIDES}" ]; then
 fi
 
 # copy files into chroot
-cp -R rootfs/. ${BUILD_PATH}/
+cp -R manifest rootfs/. ${BUILD_PATH}/
 
 # chroot into target
 mount --bind ${BUILD_PATH} ${BUILD_PATH}
 arch-chroot ${BUILD_PATH} /bin/bash <<EOF
 set -e
 set -x
+
+source /manifest
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
