54c5b9dfddef02ac0bc0fdf7189f62294682ac3d
Derek J. Clark <derkejohn.clark@gmail.com>
2022-07-07 16:51:35 -0700

    Fix delay on startup for some users, sudoers. (#350)
    * Fix delay on startup for some users. Add /etc/sudoers.d/steam for steam client root requests. Add /etc/sudoers.d/chimera for chimeraos root requests.
    
    * revert to build.sh, create files in sudoers.d

--- build.sh
+++ build.sh
@@ -128,16 +128,14 @@ passwd --lock root
 groupadd -r autologin
 useradd -m ${USERNAME} -G autologin,wheel
 echo "${USERNAME}:${USERNAME}" | chpasswd
-echo "
-root ALL=(ALL) ALL
-${USERNAME} ALL=(ALL) ALL
-${USERNAME} ALL=(ALL) NOPASSWD: /usr/bin/steamos-update
+echo "${USERNAME} ALL=(ALL) NOPASSWD: /usr/bin/steamos-update
 ${USERNAME} ALL=(ALL) NOPASSWD: /usr/bin/jupiter-biosupdate
-${USERNAME} ALL=(ALL) NOPASSWD: /usr/bin/chimera-session-use-gamescope
+${USERNAME} ALL=(ALL) NOPASSWD: /usr/bin/dmidecode -t 11
+" > /etc/sudoers.d/steam
+echo "${USERNAME} ALL=(ALL) NOPASSWD: /usr/bin/chimera-session-use-gamescope
 ${USERNAME} ALL=(ALL) NOPASSWD: /usr/bin/chimera-session-use-lightdm
 ${USERNAME} ALL=(ALL) NOPASSWD: /usr/lib/media-support/format-media.sh*
-#includedir /etc/sudoers.d
-" > /etc/sudoers
+" > /etc/sudoers.d/chimera
 
 # set the default editor, so visudo works
 echo "export EDITOR=/usr/bin/vim" >> /etc/bash.bashrc
