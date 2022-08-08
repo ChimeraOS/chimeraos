de54e692963d2774476552c03cc08525dca21d31
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2022-08-08 11:11:30 -0300

    Introduce KERNEL_PACKAGE variable to swap kernels
    We dont build the kernel package because it will take too much time. Any
    package we could put in that variable should be accessible via pacman
    repos. Pacman configuration should be edited accordingly if non-standard
    kernel is desired.

--- build.sh
+++ build.sh
@@ -111,6 +111,9 @@ Include = /etc/pacman.d/chaotic-mirrorlist
 # update package databases
 pacman --noconfirm -Syy
 
+# install kernel package
+pacman --noconfirm -S "${KERNEL_PACKAGE}" "${KERNEL_PACKAGE}-headers"
+
 # install packages
 pacman --noconfirm -S --overwrite '*' ${PACKAGES}
 rm -rf /var/cache/pacman/pkg
@@ -205,6 +208,13 @@ pacman -Q > /manifest
 # preserve installed package database
 mkdir -p /usr/var/lib/pacman
 cp -r /var/lib/pacman/local /usr/var/lib/pacman/
+
+# move kernel image and initrd to a defualt location if "linux" is not used
+if [ ${KERNEL_PACKAGE} != 'linux' ] ; then
+	mv /boot/vmlinuz-${KERNEL_PACKAGE} /boot/vmlinuz-linux
+	mv /boot/initramfs-${KERNEL_PACKAGE}.img /boot/initramfs-linux.img
+	mv /boot/initramfs-${KERNEL_PACKAGE}-fallback.img /boot/initramfs-linux-fallback.img
+fi
 
 # clean up/remove unnecessary files
 rm -rf \
