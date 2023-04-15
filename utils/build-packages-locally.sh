1f2c986b3a22be7dea4ad765b73f545e7bafb334
bouhaa <boukehaarsma23@gmail.com>
2023-04-16 00:36:42 +0200

    cleanup redundant files, add more comments to build scripts

--- utils/build-packages-locally.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-set -e
-docker pull archlinux:base-devel
-docker build --no-cache -t chimeraos-local-base:latest -f ./utils/base-system.Dockerfile .
-docker run -it --rm --entrypoint /workdir/pkgs/build-packages.sh -v $(pwd)/pkgs:/packages:Z -v $(pwd):/workdir:Z $(pwd)/.cache:/.cache chimeraos-local-base:latest
--- utils/build-packages-remote.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-set -e
-docker pull ghcr.io/chimeraos/chimeraos:master
-docker run --rm --entrypoint /workdir/pkgs/build-packages.sh -v $(pwd):/workdir:Z ghcr.io/chimeraOS/chimeraos:master
