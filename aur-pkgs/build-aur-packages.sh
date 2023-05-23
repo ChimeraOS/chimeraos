8c4e4f005ebd84d36bba767ec255b4f7a97f1b13
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2023-05-23 11:08:59 +0000

    Keep packages up to date in build
    Update packages before building AUR packages to avoid missing
    packages in mirrors.
    
    Use reflector `--age` set to 2 hours for good measure in the build
    container.

--- aur-pkgs/build-aur-packages.sh
+++ aur-pkgs/build-aur-packages.sh
@@ -10,4 +10,5 @@ sudo chown build:build /workdir/aur-pkgs
 
 PIKAUR_CMD="PKGDEST=/workdir/aur-pkgs pikaur --noconfirm -Sw ${AUR_PACKAGES}"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
+sudo pacman -Syu
 "${PIKAUR_RUN[@]}"
