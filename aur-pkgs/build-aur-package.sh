1881d09e190909d6daa391ca15e778244904666f
bouhaa <boukehaarsma23@gmail.com>
2023-05-26 14:19:57 +0200

    Remove update before building AUR package (#636)
    Updating before building AUR packages will undo package overrides.

--- aur-pkgs/build-aur-package.sh
+++ aur-pkgs/build-aur-package.sh
@@ -10,7 +10,6 @@ sudo chown build:build /workdir/aur-pkgs
 
 PIKAUR_CMD="PKGDEST=/workdir/aur-pkgs pikaur --noconfirm -Sw ${1}"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
-sudo pacman -Syu --noconfirm
 "${PIKAUR_RUN[@]}"
 # remove any epoch (:) in name, replace with -- since not allowed in artifacts
 find /workdir/aur-pkgs/*.pkg.tar* -type f -name '*:*' -execdir bash -c 'mv "$1" "${1//:/--}"' bash {} \;
