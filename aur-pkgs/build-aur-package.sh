9649517aabc7e3ab1fce84492a2866a8cb2ca540
bouhaa <boukehaarsma23@gmail.com>
2023-07-13 23:20:42 +0200

    Switch manifest to clone URL This way we can use the AUR_PACKAGES array easily to create a script for submodules on the release/ branches.

--- aur-pkgs/build-aur-package.sh
+++ aur-pkgs/build-aur-package.sh
@@ -5,10 +5,13 @@ set -x
 
 source manifest;
 
-sudo mkdir -p /workdir/aur-pkgs
+sudo mkdir -p /temp/package
+sudo chown build:build /temp/package
 sudo chown build:build /workdir/aur-pkgs
 
-PIKAUR_CMD="PKGDEST=/workdir/aur-pkgs pikaur --noconfirm -Sw ${1}"
+git clone --depth=1 https://aur.archlinux.org/${1}.git /temp/package
+
+PIKAUR_CMD="PKGDEST=/workdir/aur-pkgs pikaur --noconfirm -S -P /temp/package/PKGBUILD"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 "${PIKAUR_RUN[@]}"
 # remove any epoch (:) in name, replace with -- since not allowed in artifacts
