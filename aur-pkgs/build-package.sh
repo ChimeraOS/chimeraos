d45e28005d94e31993c92f450aa87ccda34eeeca
bouhaa <boukehaarsma23@gmail.com>
2023-09-11 21:56:38 +0200

    use gnupg directory in stable branches

--- aur-pkgs/build-package.sh
+++ aur-pkgs/build-package.sh
@@ -4,7 +4,7 @@ set -x
 
 sudo chown -R build:build /workdir/aur-pkgs
 
-PIKAUR_CMD="PKGDEST=/workdir/aur-pkgs pikaur --noconfirm -S -P /workdir/${1}/PKGBUILD"
+PIKAUR_CMD="PKGDEST=/workdir/aur-pkgs pikaur --noconfirm --build-gpgdir /etc/pacman.d/gnupg -S -P /workdir/${1}/PKGBUILD"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 "${PIKAUR_RUN[@]}"
 # remove any epoch (:) in name, replace with -- since not allowed in artifacts
