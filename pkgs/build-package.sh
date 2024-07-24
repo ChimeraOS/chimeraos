16ab338f0c49981178620e2f410b9a3039019929
Denis Benato <benato.denis96@gmail.com>
2024-07-24 14:50:25 +0200

    dracut: fix problems where package conflicting with mkinitcpio cannot be built

--- pkgs/build-package.sh
+++ pkgs/build-package.sh
@@ -4,6 +4,11 @@ set -x
 
 sudo chown -R build:build /workdir/pkgs
 
+# ChimeraOS now uses dracut and excludes mkinitcpio due to pacman hooks causing problems
+# Ensure package conflicting with mkinitcpio can be built
+sudo pacman -S --noconfirm dracut
+sudo pacman -R --noconfirm mkinitcpio
+
 PIKAUR_CMD="PKGDEST=/workdir/pkgs pikaur --noconfirm --build-gpgdir /etc/pacman.d/gnupg -S -P /workdir/${1}/PKGBUILD"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 "${PIKAUR_RUN[@]}"
