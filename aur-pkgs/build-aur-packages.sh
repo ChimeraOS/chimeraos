69240cd2ea4e9e41219cbe2bf41095d3010434bf
Denis Benato <benato.denis96@gmail.com>
2024-08-14 21:04:57 +0200

    hack: solve opengamepadui build dependency

--- aur-pkgs/build-aur-packages.sh
+++ aur-pkgs/build-aur-packages.sh
@@ -8,6 +8,8 @@ source manifest;
 sudo mkdir -p /workdir/aur-pkgs
 sudo chown build:build /workdir/aur-pkgs
 
+pikaur --noconfirm -S inputplumber-bin
+
 PIKAUR_CMD="PKGDEST=/workdir/aur-pkgs pikaur --noconfirm -Sw ${AUR_PACKAGES}"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 "${PIKAUR_RUN[@]}"
