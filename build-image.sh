aef5fc0fac1fd282a71c7012ec861f43c1f0d66c
Alesh Slovak <alesh@playtron.one>
2025-03-15 16:29:43 -0400

    workaround for jack2/pipewire-jack conflict causing build failure

--- build-image.sh
+++ build-image.sh
@@ -110,6 +110,9 @@ fi
 pacman --noconfirm -U --overwrite '*' /own_pkgs/*
 rm -rf /var/cache/pacman/pkg
 
+# remove jack2 to prevent conflict with pipewire-jack
+pacman --noconfirm -Rdd jack2 || true
+
 # install packages
 pacman --noconfirm -S --overwrite '*' --disable-download-timeout ${PACKAGES}
 rm -rf /var/cache/pacman/pkg
